af23679244904da3c44804e036d6e6807afc8bc2
Tatu Pesonen <pannariiz@gmail.com>
2020-12-29 02:16:44 +0200

    Update apps.sh

--- install/apps.sh
+++ install/apps.sh
@@ -3,9 +3,6 @@
 # Install applications
 # Comment (with #) what should not be installed and add the applications you want to install.
 
-source ./install/utils.sh
-keep_sudo_alive
-
 # Update Ubuntu
 sudo apt update
 sudo apt upgrade -y
@@ -14,25 +11,20 @@ sudo apt upgrade -y
 sudo apt -y install build-essential
 
 # Git
-sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt -y update
 sudo apt -y install git
 
 # Nodejs
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt install -y nodejs npm
 
 # Nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt -y update && sudo apt -y install yarn
-
-# Ubuntu WSL
-# Check out https://github.com/wslutilities/wslufor more details
-sudo apt install ubuntu-wsl
 
 # Finish
 e_success "Finished applications installation."
